f6684dbd73597072c3a99e1c1075eaa1c5c3cf33
AngelGaelGarciaRangel <105666768+AngelGaelGarciaRangel@users.noreply.github.com>
2022-08-13 18:45:57 -0500

    Dijkstra's code and rest of his visualization

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ wwidth = 500
 wheight = 500
 window = pygame.display.set_mode((wwidth, wheight))
 #Create all the necessary for the grid
-col = 25
-row = 25
+col = 50
+row = 50
 bwidth = wwidth // col
 bheight = wheight // row
 grid = []
+#queue that will have the nodes
+queue = []
+#array that will have the pat
+path = []
 #Create the class that will be helping to make the grid
 class Box:
   def __init__(self, i, j):
@@ -21,21 +25,50 @@ class Box:
     self.start = False
     self.end = False
     self.obstacle = False
+    #if the box is already on the queue or not
+    self.queue = False
+    #if the box have been visited
+    self.visited = False
+    #neighbours of each box
+    self.neighbours = []
+    self.prior = None
   def draw(self, w, color):
     pygame.draw.rect(w, color, (self.x *bwidth, self.y * bheight, bwidth - 2, bheight - 2))
+  def set_neighbours(self):
+    #if the box have an horizontal left neighbour, add it
+    if self.x > 0:
+      self.neighbours.append(grid[self.x - 1][self.y])
+    #if the box have an horizontal right neighbour, add it
+    if self.x < col - 1:
+      self.neighbours.append(grid[self.x + 1][self.y])
+    #if the box have a vertical up neighbour, add it
+    if self.y > 0:
+      self.neighbours.append(grid[self.x][self.y - 1])
+    #if the box have a vertical down neighbour, add it  
+    if self.y < row - 1:
+      self.neighbours.append(grid[self.x][self.y + 1])
 #Create the grid by iterate over all the possible positions
 for i in range(col):
   array = []
   for j in range(row):
     array.append(Box(i, j))
   grid.append(array)
+#Set the neighbours for all the boxes
+for i in range(col):
+  for j in range(row):
+    grid[i][j].set_neighbours()
+
 start_box = grid[0][0]
 start_box.start = True
+#put the start_box into the queue
+queue.append(start_box)
+start_box.visited = True
 #Specify the main function
 def main():
   begin_search = False
   end_box_set = False
   end_box  = None
+  searching = True
   while True:
     for event in pygame.event.get():
       #If user wants to exit
@@ -61,6 +94,27 @@ def main():
       #If user put the end of the path, start the algorithm
       if event.type == pygame.KEYDOWN and end_box_set:
         begin_search = True
+    if begin_search:
+      if len(queue) > 0 and searching:
+        current_box = queue.pop(0)
+        current_box.visited = True
+        if current_box == end_box:
+          searching = False
+          while current_box.prior != start_box:
+            path.append(current_box.prior)
+            current_box = current_box.prior
+        else:
+          for neighbour in current_box.neighbours:
+            if not neighbour.queue and not neighbour.obstacle:
+              neighbour.queue = True
+              neighbour.prior = current_box
+              queue.append(neighbour)
+      #if queue is 0 and we never found the solution
+      else:
+        if searching:
+          Tk().wm_withdraw()
+          messagebox.showinfo("No solution", "There is no solution")
+          searching = False
    #Specify the color of the box
     window.fill((0, 0, 0))
    #Draw the grid 
@@ -68,6 +122,12 @@ def main():
       for j in range(row):
         box = grid[i][j]
         box.draw(window, (50, 50, 50))
+        if box.queue:
+          box.draw(window, (200, 0, 0))
+        if box.visited:
+          box.draw(window, (0, 200, 0))
+        if box in path:
+          box.draw(window, (0, 0, 200))
         if box.start:
           box.draw(window, (0, 200, 200))
         if box.end:
@@ -77,3 +137,4 @@ def main():
    #Update the display
     pygame.display.flip()
 main()
+
